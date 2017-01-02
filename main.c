8a8325cfd3e372baf64673d33d84cda6dee38bfe
tanligao <tanligaogao@gmail.com>
2017-01-02 17:53:07 +0800

    test the miniftpd config

--- main.c
+++ main.c
@@ -53,22 +53,26 @@ int main(int argc,char *argv[])
 	*/
 
 	// test for parseconf
-	/*
+
 	parseconf_load_file("miniftpd.conf");
-	printf("%d\n", tunable_pasv_enable);
-	printf("%d\n", tunable_port_enable);
-	printf("%d\n", tunable_listen_port);
-	printf("%d\n", tunable_max_clients);
-	printf("%d\n", tunable_max_per_ip);
-	printf("%d\n", tunable_accept_timeout);
-	printf("%d\n", tunable_connect_timeout);
-	printf("%d\n", tunable_idle_session_timeout);
-	printf("%d\n", tunable_data_connection_timeout);
-	printf("%o\n", tunable_local_umask);
-	printf("%d\n", tunable_upload_max_rate);
-	printf("%d\n", tunable_download_max_rate);		
-	*/
-	
+	printf("tunable_pasv_enable: %d\n", tunable_pasv_enable);
+	printf("tunable_port_enable: %d\n", tunable_port_enable);
+	printf("tunable_listen_port: %d\n", tunable_listen_port);
+	printf("tunable_max_clients: %d\n", tunable_max_clients);
+	printf("tunable_max_per_ip: %d\n", tunable_max_per_ip);
+	printf("tunable_accept_timeout: %d\n", tunable_accept_timeout);
+	printf("tunable_connect_timeout: %d\n", tunable_connect_timeout);
+	printf("tunable_idle_session_timeout: %d\n", tunable_idle_session_timeout);
+	printf("tunable_data_connection_timeout: %d\n", tunable_data_connection_timeout);
+	printf("tunable_local_umask: 0%o\n", tunable_local_umask);
+	printf("tunable_upload_max_rate: %d\n", tunable_upload_max_rate);
+	printf("tunable_download_max_rate: %d\n", tunable_download_max_rate);		
+
+	if( tunable_listen_adress == NULL )
+		printf("tunable_listen_addrss = NULL\n");
+	else
+		printf("tunable_listen_addrss: %s\n", tunable_listen_adress);
+
 	int listenfd = tcp_server(NULL,8888);
 	
 	session_t sess = {-1,"","","",-1,-1};
